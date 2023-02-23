c7054ce4a17e172c7f349b8d403ae687179133e6
sawonhossain34 <sawonhossain3491@gmail.com>
2023-02-23 21:35:32 +0600

    first commit

---
+++ js/quotes.js
@@ -0,0 +1,15 @@
+const loadQuote = () =>{
+    fetch('https://api.kanye.rest/')
+    .then(res => res.json())
+    .then(data => displayQuote(data))
+}
+
+const displayQuote = quote =>{
+
+const blockQuote = document.getElementById('quote');
+// console.log(quote.quote);
+blockQuote.innerHTML = quote.quote;
+}
+
+
+loadQuote();
