de0bb31b8cfbabf9fb951fe583e17ea96315ee68
sawonhossain34 <sawonhossain3491@gmail.com>
2023-02-24 12:10:53 +0600

    name and country img

--- js/countries.js
+++ js/countries.js
@@ -11,15 +11,33 @@ const displayCountries = countries => {
     //     console.log(countries);
     // }
     countries.forEach(country => {
-        console.log(country.name.common);
+        // console.log(country.cca2);
         const countryDiv =document.createElement('div');
         countryDiv.classList.add('country');
         countryDiv.innerHTML = `
         <h3>Name :${country.name.common}</h3>
         <p>Capital:${country.capital ? country.capital[0] : 'no capital'}</p>
+        <button onclick = "loadCountryDetails('${country.cca2}')">Details</button>
+        
         `;
         countriesContainer.appendChild(countryDiv);
     });
 }
+const loadCountryDetails = code =>{
+    // https://restcountries.com/v2/alpha/{code}
+    const url = `https://restcountries.com/v2/alpha/${code}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showCountryDetails(data))
+}
 
+const showCountryDetails = country =>{
+console.log(country.name);
+const detailsContainer =document.getElementById('const details');
+detailsContainer.innerHTML =`
+<h3>Name:${country.name} </h3>
+<img src = "${country.flags.png}">
+`
+
+}
 loadCountries();
